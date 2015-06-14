c6d20bd5bec642347d2cf32d9df40c525bea617d
Alexander Chebykin <a.e.chebykin@gmail.com>
2015-06-14 11:12:03 +0300

    Working solution, incorrect answer

--- StarMap/Program.fs
+++ StarMap/Program.fs
@@ -3,7 +3,7 @@ let tuplePlus (a,b) (c,d) =
   (a + b, c + d)
 let tupleNeg (a,b) =
   (- a, -b)
-let eps = 0.1
+
 (*
 let calcMotion (x, y) (x', y') (x1, y1) (x1', y1') =
   //let i = sin (System.Math.PI / 2.0)
@@ -78,8 +78,8 @@ let calcMotion2 (x, y) (x', y') (x1, y1) (x1', y1') eps =
     else
       if (abs (x1' - (x1 * ang1cN - y1 * ang1s + x0_1N)) < eps) && (abs (y1' - (x1 * ang1s + y1 *  ang1cN + y0_1N)) < eps) then 
           res <- (ang1s, ang1cN, x0_1N, y0_1N)
-      else
-        if (abs (x1' - (x1 * ang2cN - y1 * ang2s + x0_2N)) < eps) && (abs (y1' - (x1 * ang2s + y1 * ang2cN + y0_2N)) < eps) then 
+     // else
+      if (abs (x1' - (x1 * ang2cN - y1 * ang2s + x0_2N)) < eps) && (abs (y1' - (x1 * ang2s + y1 * ang2cN + y0_2N)) < eps) then 
           res <- (ang2s, ang2cN, x0_2N, y0_2N)
   res
 
@@ -98,44 +98,90 @@ let stringToPoint (s : string) =
     i <- i + 1
   ((float) x, (float) y)
 
-let length (a,b) (c,d) = 
+let length (a : float ,b : float) (c : float,d : float)  = 
   sqrt (pown (a - c) 2 + pown (b - d) 2)
 
+let motionPoint (x,y) sin cos x0 y0 = 
+  let x' = x * cos - y * sin + x0
+  let y' = x * sin + y * cos + y0
+  (x', y')
+  
 [<EntryPoint>]
 let main argv = 
-  let in1 = new System.IO.StreamReader ("in1.txt")
+  let (sin, cos, x0, y0) = calcMotion2 (3.0,3.0) (20.0,4.0) (3.0,1.0) (20.0,6.0) 0.1
+  printfn "%A" (motionPoint (1.0,1.0) sin cos x0 y0)
+  (*
+  let in1 = new System.IO.StreamReader ("in.txt")
   let pointDelta = (float) (in1.ReadLine ())
   let arcDelta = (float) (in1.ReadLine ())
   let mutable buffer = in1.ReadLine ()
   let mutable pointsBuf = []
-  while (buffer <> null) do
+  while (buffer <> "Next map") do
     let point = stringToPoint buffer
     pointsBuf <- point :: pointsBuf
     buffer <- in1.ReadLine ()
   let map1 = List.toArray pointsBuf //List will be reversed, but it doesn't matter for us. Next times too.
     //'map' stands for 'star map'
-  let in2 = new System.IO.StreamReader ("in2.txt")
   pointsBuf <- []
-  buffer <- in2.ReadLine ()
+  buffer <- in1.ReadLine ()
   while (buffer <> null) do
     let point = stringToPoint buffer
     pointsBuf <- point :: pointsBuf
-    buffer <- in2.ReadLine ()
-  let map2 = List.toArray pointsBuf 
+    buffer <- in1.ReadLine ()
+  let map2 = Array.sort (List.toArray pointsBuf )
 
   let mutable arcBuffer = []
-  for i in map1 do
-   for j in map1 do
-    arcBuffer <- (i, j, length i j) :: arcBuffer
-  let arc1 = List.toArray arcBuffer
+  for i = 0 to map1.Length - 1 do
+   for j = i to map1.Length - 1 do
+    arcBuffer <- (length map1.[i] map1.[j], (map1.[i], map1.[j])) :: arcBuffer
+      //Complicated structure (length, (point, pont)) is used so that 'fst' and 'snd' commands could be used
+  let arcs1 = List.toArray arcBuffer
 
   arcBuffer <- []
-  for i in map2 do
-   for j in map2 do
-    arcBuffer <- (i, j, length i j) :: arcBuffer
-  let arc2 = List.toArray arcBuffer
+  for i = 0 to map2.Length - 1 do
+   for j = i + 1 to map2.Length - 1 do
+    arcBuffer <- (length map2.[i] map2.[j], (map2.[i], map2.[j])) :: arcBuffer
+  let arcs2 = Array.sort (List.toArray arcBuffer)
+  
+  let mutable res = 0
+  for arc in arcs1 do
+    let mutable m = arcs2.Length / 2
+    let arcs2len = arcs2.Length
+    let mutable mChange = 1
+    while abs(fst arc- fst arcs2.[m]) > arcDelta && mChange <> 0 do
+      let mOld = m
+      if (fst arc < fst arcs2.[m]) then m <- m / 2
+      else m <- m + (arcs2len - m) / 2
+      mChange <- mOld - m
+    let mutable sndArc = m
+    while abs(fst arc - fst arcs2.[sndArc]) < arcDelta && sndArc > 0 do
+      sndArc <- sndArc - 1
+    while abs(fst arc - fst arcs2.[sndArc]) < arcDelta do
+      let mutable count = 0
+      let (sin, cos, x0, y0) = calcMotion2 (fst (snd arc)) (fst (snd arcs2.[sndArc])) (snd (snd arc)) (snd (snd arcs2.[sndArc])) pointDelta
+      for i in map1 do
+        let fstPoint = motionPoint i sin cos x0 y0
+        let map2len = map2.Length
+        let mutable mpoint = map2.Length / 2
+        let mutable mpointChange = 1
+        while abs (fst fstPoint - fst map2.[mpoint]) > pointDelta  && mpointChange <> 0 do
+          let mpointOld = mpoint
+          if (fst fstPoint < fst map2.[mpoint]) then mpoint <- mpoint / 2
+          else mpoint <- mpoint + (map2len - mpoint) / 2
+          mpointChange <- mpointOld - m
+        while abs (fst fstPoint - fst map2.[mpoint]) < pointDelta && mpoint > 0 do
+          mpoint <- mpoint - 1
 
-  let arc1 = Array.sort arc1
+        let mutable notFoundFlag = true
+        while abs (fst fstPoint - fst map2.[mpoint]) < pointDelta && notFoundFlag && mpoint < map2.Length do
+          if length fstPoint map2.[mpoint] < pointDelta then
+            count <- count + 1
+            notFoundFlag <- false
+          mpoint <- mpoint + 1
+      if (count > res) then res <- count
+      sndArc <- sndArc + 1
+  printfn "%A" res
+  *)
   (*
   for i in map1 do
     printf "%A " i
